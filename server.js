cd6493a0db2653afc34a2fdd7680025ba0a940c1
max254 <maxkamau77@gmail.com>
2025-08-07 18:03:33 +0300

    .

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const hostname = process.env.HOST;
+
+const port = Process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('welcome to pearl server');
+});
+
+app.listen(port, () =>{
+  console.log(`server running at http:${hostname}: ${port}/`);
+  console.log(`press ctrl + C to stop server.`);
+  console.log(`press ctrl + R to restart server.`);
+  console.log(`waiting for requests...`);
+}
